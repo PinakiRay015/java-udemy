65a127c4e75d85203d7181b730e8fce8a252903d
Pinaki Ray <pinaki.bpd2001@gmail.com>
2022-01-27 15:21:03 +0530

    java

--- input/input.java
+++ input/input.java
@@ -4,9 +4,12 @@ public class input {
     
     public static void main(String[] args) {
         Scanner java = new Scanner(System.in);
-        System.out.print("Enter the number you want to print ");
+        System.out.println("Enter your name");
+        String name = java.nextLine();
+        System.out.println("Enter the number");
         int a = java.nextInt();
-        System.out.println("The number you entered is "+a);
+        System.out.println("This is your name "+name);
+        System.out.println("This is your entered number "+a);
         java.close();
     }
 }
